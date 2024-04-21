2efbc428ca1c9e02957137596279f255d8f6b448
Vanv1k <ivanspecial5@gmail.com>
2024-04-21 12:22:26 +0300

    hotfix

--- src/widgets/Toggle/Toggle.jsx
+++ src/widgets/Toggle/Toggle.jsx
@@ -4,7 +4,7 @@ import './styles.css'
 export const Toggle = ({isVideoEnabled, handleToggleVideo}) => {
     return (
         <>
-            <label className="video-toggle" htmlFor="video-toggle">Получать видео:</label>
+            <label className="video-toggle" htmlFor="video-toggle">Получать видеопоток:</label>
             <input
                 id="video-toggle"
                 type="checkbox"
